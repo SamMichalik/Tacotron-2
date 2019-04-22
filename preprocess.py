0622d528f3209ce3837370ed044c79a8dd872adb
Samuel Michalik <samuelmichalik@yahoo.com>
2019-04-22 11:26:36 +0200

    adapt for own dataset

--- preprocess.py
+++ preprocess.py
@@ -44,6 +44,8 @@ def norm_data(args):
 	if args.dataset.startswith('LJSpeech'):
 		return [os.path.join(args.base_dir, args.dataset)]
 
+	if args.dataset == 'gnd_dan_brown':
+		return [os.path.join(args.base_dir, args.dataset)]
 
 	if args.dataset == 'M-AILABS':
 		supported_languages = ['en_US', 'en_UK', 'fr_FR', 'it_IT', 'de_DE', 'es_ES', 'ru_RU',
@@ -89,7 +91,7 @@ def main():
 	parser.add_argument('--base_dir', default='')
 	parser.add_argument('--hparams', default='',
 		help='Hyperparameter overrides as a comma-separated list of name=value pairs')
-	parser.add_argument('--dataset', default='LJSpeech-1.1')
+	parser.add_argument('--dataset', default='gnd_dan_brown')
 	parser.add_argument('--language', default='en_US')
 	parser.add_argument('--voice', default='female')
 	parser.add_argument('--reader', default='mary_ann')
